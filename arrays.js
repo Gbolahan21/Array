dcb2b9caaf94adecba749dc77a3e1d5ce3dcd008
gbolahan <mo@carrotcredit.com>
2025-01-31 12:31:42 +0100

    array

---
+++ arrays.js
@@ -0,0 +1,20 @@
+let arr = [
+    {
+        'name': 'sodiq',
+        'age': '67'
+    },
+    {
+        'name': 'usman',
+        'age': '97'
+    },
+    {
+        'name': 'khadijah',
+        'age': '107'
+    }
+]
+
+{
+    arr.map(item => (
+        console.log(item.name)
+    ))
+}
